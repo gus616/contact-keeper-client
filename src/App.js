ceb53f7040a1e460f6e21d22df4801281b6de035
gus616 <guzz.vc@gmail.com>
2020-10-24 20:32:46 -0500

    Se añaden componentes Login, Register y lógica en Context

--- src/App.js
+++ src/App.js
@@ -4,22 +4,40 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
+
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
+
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
 const App = () => {
   return (
+    <AuthState>
     <ContactState>
-    <Router>
-      <Fragment>
-        <Navbar/>
-        <div className="container">
-          <Switch>
-             <Route exact path='/' component={Home}/>
-             <Route exact path='/about' component={About}/>
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+      <AlertState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Alerts />
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/register' component={Register}/>
+              <Route exact path='/login' component={Login}/>
+              <Route exact path='/about' component={About}/>
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+      </AlertState>
     </ContactState>
+    </AuthState>
   );
 }
 
---
+++ src/context/auth/AuthState.js
@@ -0,0 +1,105 @@
+import React, {useReducer} from 'react';
+import authReducer from '../auth/authReducer';
+import AuthContext from '../auth/authContext';
+import axios from 'axios';
+import setAuthToken from '../../utils/setAuthToken';
+
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT, CLEAR_ERRORS
+} from '../types';
+
+const AuthState = props => {
+    
+    const initialState = {
+        token: localStorage.getItem('token'),
+        isAuthenticated: null,
+        loading: true,
+        user: null,
+        error: null
+    };
+    const [state, dispatch] = useReducer(authReducer, initialState);
+
+    //Load user
+    const loadUser = async () => {
+        //if(localStorage.token){
+            setAuthToken(localStorage.token);
+      //  }
+        try{
+            const url = 'http://localhost:5000/api/auth';
+            const res = await axios.get(url);
+
+            dispatch({ 
+                type: USER_LOADED, 
+                payload: res.data 
+            });            
+        } catch(err){
+            dispatch({
+                type: AUTH_ERROR
+            })
+        }
+    };
+
+    //Register user
+    const register = async formData => {
+        const config = {
+            headers:{
+                'Content-Type':'application/json'
+            }
+        }
+
+        try{
+            const url = 'http://localhost:5000/api/users';
+            const res = await axios.post(url, formData, config);
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            });
+
+            loadUser();
+        } catch(err){
+            dispatch({
+                type: REGISTER_FAIL,
+                payload: err.response.data.msg
+            });
+        }
+    };
+
+    //Login user
+    const login = () => console.log('login user');
+
+    //Logout user
+    const logout = () => console.log('logout user');
+    //Clear Errors
+    const clearErrors = () =>dispatch({ type: CLEAR_ERRORS });
+
+    return(
+        <AuthContext.Provider 
+        value={{
+            token: state.token,
+            isAuthenticated: state.isAuthenticated,
+            loading: state.loading,
+            user: state.user,
+            error: state.error,
+            loadUser,
+            register,
+            login,
+            logout,
+            clearErrors
+        }}>
+            {props.children}
+        </AuthContext.Provider>);        
+};
+
+export default AuthState;
+
+
+
+
+
+
